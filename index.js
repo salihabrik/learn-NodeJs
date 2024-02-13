66e712dda45f2fe89f6f2a718199a8045f6342e1
Software Engineering Student <briksaliha@gmail.com>
2024-02-13 14:05:57 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 
 const app = express();
+app.get('/', (req, res) => {
 
-app.get('/Hello', (req, res) => {
-
-    res.send("Hello");
+    res.send("hello this is my first project nodejs ");
 })
-app.get('/test', (req, res) => {
+app.post('/Hello', (req, res) => {
+
+    res.send("Hello from post");
+})
+app.put('/test', (req, res) => {
 
     res.send("you can visited test ");
 })
+app.delete('/test', (req, res) => {
+    res.send("you can delete test ");
+})
+
     app.get('/hi', (req, res) => {
-        res.send(`hi you can visited`); 
+        let numbers = "";
+        for (let i = 0; i < 100; i++) {
+            numbers += i + '|';
+        }
+res.send(`hi you can visited hi the numbers are :${numbers})`); 
     });
+
+
     app.listen(3000, () => {
 
     console.log("Server running on port 3000");
